89e134c9f38cc824f7a0fcf56746e3d1e5b8e3cd
Panuwattor <panuwat.taweechai@gmail.com>
2022-07-19 19:56:16 +0700

    chart

---
+++ vw_chart.php
@@ -0,0 +1,118 @@
+<section class="site-container">
+            <div class="auth-page">
+                <div class="container">
+                        <div class="h-title typo-lg fw-medium text-center">
+                            Chart
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                              <!-- AREA CHART -->
+                              <div class="card mt-5">
+                                <div class="card-header">
+                                  <h3 class="card-title">Area Chart</h3>
+                                </div>
+                                <div class="card-body">
+                                  <div class="chart">
+                                    <canvas id="areaChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                                  </div>
+                                </div>
+                                <!-- /.card-body -->
+                              </div>
+                              <!-- /.card -->
+                  
+                              <!-- DONUT CHART -->
+                              <div class="card mt-5">
+                                <div class="card-header">
+                                  <h3 class="card-title">Donut Chart</h3>
+                                </div>
+                                <div class="card-body">
+                                  <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                                </div>
+                                <!-- /.card-body -->
+                              </div>
+                              <!-- /.card -->
+                  
+                              <!-- PIE CHART -->
+                              <div class="card mt-5">
+                                <div class="card-header">
+                                  <h3 class="card-title">Pie Chart</h3>
+                                </div>
+                                <div class="card-body">
+                                  <canvas id="pieChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                                </div>
+                                <!-- /.card-body -->
+                              </div>
+                              <!-- /.card -->
+                  
+                            </div>
+                            <!-- /.col (LEFT) -->
+                            <div class="col-md-6">
+                              <!-- LINE CHART -->
+                              <div class="card mt-5">
+                                <div class="card-header">
+                                  <h3 class="card-title">Line Chart</h3>
+                                </div>
+                                <div class="card-body">
+                                  <div class="chart">
+                                    <canvas id="lineChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                                  </div>
+                                </div>
+                                <!-- /.card-body -->
+                              </div>
+                              <!-- /.card -->
+                  
+                              <!-- BAR CHART -->
+                              <div class="card mt-5">
+                                <div class="card-header">
+                                  <h3 class="card-title">Bar Chart</h3>
+                                </div>
+                                <div class="card-body">
+                                  <div class="chart">
+                                    <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                                  </div>
+                                </div>
+                                <!-- /.card-body -->
+                              </div>
+                              <!-- /.card -->
+                  
+                              <!-- STACKED BAR CHART -->
+                              <div class="card mt-5">
+                                <div class="card-header">
+                                  <h3 class="card-title">Stacked Bar Chart</h3>
+                                </div>
+                                <div class="card-body">
+                                  <div class="chart">
+                                    <canvas id="stackedBarChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                                  </div>
+                                </div>
+                                <!-- /.card-body -->
+                              </div>
+                              <!-- /.card -->
+
+                                <!-- STACKED BAR CHART -->
+                                <div class="card mt-5">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Horizontal Bar Chart</h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="chart">
+                                        <canvas id="horizontalBarChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                                        </div>
+                                    </div>
+                                    <!-- /.card-body -->
+                                    </div>
+                                    <!-- /.card -->
+                            </div>
+                            <!-- /.col (RIGHT) -->
+                          </div>
+                </div>
+            </div>
+        </section>
+        
+        </div>
+
+
+<!-- script -->
+<!-- มี js  ใน js/apps/chart.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.0/Chart.bundle.js"></script>
+<!-- script -->
