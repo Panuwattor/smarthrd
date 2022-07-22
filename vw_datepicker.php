b891d988afaf4d248d304d4ec1c5a4ba1381213a
Panuwattor <panuwat.taweechai@gmail.com>
2022-07-22 21:18:00 +0700

    test

---
+++ vw_datepicker.php
@@ -0,0 +1,40 @@
+
+        <section class="site-container">
+            <div class="auth-page">
+                <div class="container">
+                    <div class="default-filter">
+                        <div class="card">
+                            <div class="card-header">
+                                <h2 class="title">Datepicker</h2>
+                            </div>
+                            <div class="card-body">
+                                <form class="form-default">
+                                    <div class="row gutters-10 align-items-center">
+                                        <div class="col">
+                                            <label>default(en)</label>
+                                            <input class=" form-control input-medium" type="text" data-provide="datepicker">
+                                        </div>
+                                        <div class="col">
+                                            <label>th</label>
+                                            <input class=" form-control input-medium" type="text" data-provide="datepicker" data-date-language="th">
+                                        </div>
+                                        <div class="col">
+                                            <label>th-th</label>
+                                            <input class=" form-control input-medium" type="text" data-provide="datepicker" data-date-language="th-th">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        </div>
+
+
+<!-- script -->
+<!-- มี js  ใน js/apps/datepicker_th.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+<!-- script -->
